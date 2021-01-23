a726faf800016582c6300e7a553a301361abf4ee
Arnas Montrimas <marnas02@gmail.com>
2021-01-23 12:06:06 +0000

    changing query from MySQL TO PostgreSql

--- app/Services/ExcelData.php
+++ app/Services/ExcelData.php
@@ -17,9 +17,10 @@ class ExcelData {
             round((((e.salary/52)/48) * sum((t.monday + t.tuesday + t.wednesday + t.thursday + t.friday + t.saturday))), 2) as "Cost To Build",
             round((p.selling_price - (((e.salary/52)/48) * sum((t.monday + t.tuesday + t.wednesday + t.thursday + t.friday + t.saturday)))), 2) as "Net Profit"
             FROM products p, timesheets t, employees e 
-            WHERE t.product_id = p.id AND t.employee_id = e.id
-            AND t.weekEnding BETWEEN ? AND ?
-            AND p.status = ? GROUP BY p.id, p.name, p.type, t.employee_id, p.build_time, p.selling_price, e.salary',
+            WHERE t.product_id = p.id 
+            AND t.employee_id = e.id
+            AND t.weekEnding BETWEEN ? AND ? AND p.status = ? 
+            GROUP BY p.id, p.name, p.type, t.employee_id, p.build_time, p.selling_price, e.salary',
         [$date1, $date2, 'completed']);
 
         if($excelData != null) {
